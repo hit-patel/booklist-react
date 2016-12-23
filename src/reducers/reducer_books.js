0e41fe310a69fe71c5eac05c18a62f4b097ba5e6
hitesh patel <hitesh patel>
2016-12-23 14:11:55 -0800

    commited booklist react

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function (){
+  return [
+    {title:'Javascript: The Good Parts',pages:101},
+    {title:'Harry Potter',pages:39},
+    {title:'The Dark Tower',pages:85},
+    {title:'Eloquent Baby',pages:1}
+  ]
+}
+
+// export default function
